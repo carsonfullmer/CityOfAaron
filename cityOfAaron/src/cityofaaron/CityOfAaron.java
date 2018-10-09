06eee7e32178e4909b1b272bb6f8aa7d96ad3aa9
Carson Fullmer <cfullmer6@gmail.com>
2018-10-09 16:02:18 -0600

    10/9/2018 L04: Team Assignment - Creating cityOfAaron repository

---
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -0,0 +1,22 @@
+/*
+ * The main() class file for the cityOfAaron project
+ * CIT-260
+ * Fall 2018
+ * Team members: Carson Fullmer, Austin Laibly, Taylor Mackintosh
+ */
+package cityofaaron;
+
+/**
+ *
+ * @author cfull
+ */
+public class CityOfAaron {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
